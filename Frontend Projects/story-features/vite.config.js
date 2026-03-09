45da45515946d981e9dc93787f4bf384fd01ab18
Lecel Echavarre <161615845+lecelechavarre@users.noreply.github.com>
2026-03-09 16:27:57 +0800

    Add files via upload

---
+++ Frontend Projects/story-features/vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
+
+// https://vitejs.dev/config/
+module.exports = defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
+})
